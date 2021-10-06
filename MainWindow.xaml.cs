fc54fe07c24902a7daa9875fc996f0b755228fb3
Benjamin Boyce <boyceb@gmail.com>
2021-10-06 12:16:45 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InterfacePrototype
+{
+
+    public partial class MainWindow : Window
+    {
+        public Player player;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            Player player = new Player();
+            GameText.Text = "Welcome to the game. You are now " +
+                "facing " + player.CurrentDirection;
+            
+        }
+
+        private void North_Click(object sender, RoutedEventArgs e)
+        {
+            if (player.CurrentDirection == Directions.North)
+            {
+                player.CurrentDirection = Directions.North;
+                GameText.Text = "You are now facing North";
+
+            }
+
+            else
+            {
+                GameText.Text += "the direction was not north";
+            }
+
+
+
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace InterfacePrototype
+{
+
+    public enum Directions
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public class Player : INotifyPropertyChanged
+    {
+
+
+        public Directions CurrentDirection;
+        
+        
+         
+        public Player()
+        {
+            this.CurrentDirection = Directions.South;
+
+
+        }
+
+        //methods
+
+    }
+}
